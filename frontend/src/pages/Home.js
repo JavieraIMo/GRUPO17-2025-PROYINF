f85fa707d0a8df51b44b303507cded560fc2747e
anto-toisa <asalinasl@usm.cl>
2025-10-30 00:47:46 -0300

    Ejemplos de comentarios para documentar

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,3 +1,17 @@
+// Ejemplos de comentarios (breve guía):
+// JavaScript (fuera de JSX):
+//   // comentario de una línea
+//   /* comentario
+//      multilínea */
+// JSX (dentro del return/JSX):
+//   {/* comentario en JSX */}
+// CSS:
+//   /* comentario CSS */
+// HTML:
+//   <!-- comentario HTML -->
+// SQL:
+//   -- comentario SQL
+//   /* comentario SQL multilínea */
 import React from 'react';
 import './Home.css';
 import LoanSimulator from '../components/LoanSimulator/LoanSimulator';
@@ -9,6 +23,7 @@ function Home() {
         <h1>Bienvenido a Alara Simulador</h1>
         <p>Calcula tus préstamos de consumo de manera fácil y rápida.</p>
         <button className="btn-comenzar">Comenzar</button>
+        
       </section>
     </div>
   );
